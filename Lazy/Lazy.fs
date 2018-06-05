bf25febd1415423fd5d907ec155fbc8db04e2bf5
Гурьев Василий Александрович <Vaschudo@gmail.com>
2018-06-05 22:15:15 +0300

    Add "lazy" proj and tests.

---
+++ Lazy/Lazy.fs
@@ -0,0 +1,65 @@
+﻿open System
+open System.Threading
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleThreadedLazy<'a> (supplier : unit -> 'a) =
+    let mutable isFirst = true
+    let mutable result =  Unchecked.defaultof<'a>
+    interface ILazy<'a> with
+        member this.Get() = 
+            if (isFirst)
+            then
+                isFirst <- false
+                result <- supplier()
+            result
+   
+type MultiThreadedLazy<'a> (supplier : unit -> 'a) =
+    let mutable isFirst = true
+    let mutable result =  Unchecked.defaultof<'a>
+    let lockObj = new Object()
+
+    interface ILazy<'a> with
+        member this.Get() = 
+            if (isFirst)
+            then 
+                lock lockObj (fun() ->
+                    if (isFirst)
+                    then
+                        isFirst <- false
+                        result <- (supplier())
+                )
+            result   
+
+type MultiThreadedLockFreeLazy<'a when 'a : not struct and 'a : equality> (supplier : unit -> 'a) =
+    let mutable isFirst = true
+    let mutable result =  Unchecked.defaultof<'a>
+
+    interface ILazy<'a> with
+        member this.Get() = 
+            if (Volatile.Read(&isFirst) = true)
+            then
+                let mutable compRes = supplier()
+                let mutable curRes = result
+
+                while (Interlocked.CompareExchange(&result, compRes, curRes) <> curRes) do
+                    compRes <- supplier()
+                    curRes <- result
+
+            Volatile.Write(&isFirst, false)
+            result
+        
+type LazyFactory =
+    static member CreateSingleThreadedLazy (supplier : unit -> 'a) = 
+        new SingleThreadedLazy<'a> (supplier) :> ILazy<'a>
+
+    static member CreateMultiThreadedLazy (supplier : unit -> 'a) =
+        new MultiThreadedLazy<'a> (supplier) :> ILazy<'a>
+    
+    static member CreateMultiThreadedLockFreeLazy (supplier : unit -> 'a) =
+        new MultiThreadedLockFreeLazy<'a> (supplier) :> ILazy<'a>
+
+[<EntryPoint>]
+let main argv =
+    0
---
+++ Test/Test.fs
@@ -0,0 +1,55 @@
+﻿open Lazy
+open NUnit.Framework
+open FsUnit
+open System.Threading.Tasks
+
+[<Test>]
+let ``test of SingleThreadedLazy class 1``() =
+    let mutable i = 0
+    let c = LazyFactory.CreateSingleThreadedLazy (fun() -> i <- i + 1)
+    c.Get()
+    c.Get()
+    i |> should equal 1
+
+
+[<Test>]
+let ``test of SingleThreadedLazy class 2``() =
+    let c = LazyFactory.CreateSingleThreadedLazy (fun() -> [1..10] |> List.sum)
+    c.Get() |> should equal 55
+
+[<Test>]
+let ``test of MultiThreadedLazy class 1``() =
+    let mutable i = 0
+    let c = LazyFactory.CreateMultiThreadedLazy (fun() -> i <- i + 1)
+    Parallel.For(1, 30, fun num -> c.Get()) |> ignore
+    i |> should equal 1
+
+[<Test>]
+let ``test of MultiThreadedLazy class 2``() =
+    let c = LazyFactory.CreateMultiThreadedLazy (fun() -> [1..10] |> List.sum)
+    c.Get() |> should equal 55
+
+[<Test>]
+let ``test of MultiThreadedLockFreeLazy class 1``() =
+    let mutable i = 0
+    let c = LazyFactory.CreateMultiThreadedLockFreeLazy (fun() -> i <- i + 1)
+    Parallel.For(1, 30, fun num -> c.Get()) |> ignore
+    i |> should equal 1
+
+[<Test>]
+let ``test of MultiThreadedLockFreeLazy class 2``() =
+    let c = LazyFactory.CreateMultiThreadedLockFreeLazy (fun() -> [1..10] |> List.map (fun i -> i))
+    c.Get() |> List.sum |> should equal 55
+
+
+[<EntryPoint>]
+let main argv =
+    ``test of SingleThreadedLazy class 1``()
+    ``test of SingleThreadedLazy class 2``()
+
+    ``test of MultiThreadedLazy class 1``()
+    ``test of MultiThreadedLazy class 2``()
+
+    ``test of MultiThreadedLockFreeLazy class 1``()
+    ``test of MultiThreadedLockFreeLazy class 2``()
+    0 
